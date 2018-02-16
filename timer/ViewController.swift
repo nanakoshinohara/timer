d9f83e6669e41f9f0cd55656563e3061fb9e8a36
篠原七子 <Nanako@shinoharananako-no-MacBook-Air.local>
2018-02-16 10:42:03 +0900

    Initial Commit

---
+++ timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by 篠原七子 on 2018/02/16.
+//  Copyright © 2018年 篠原七子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
